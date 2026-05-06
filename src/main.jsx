3339153308f28e296f3386b275428daf4a1e95a8
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-07 01:29:08 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import AuthConteext from './context/AuthConteext.jsx'
 import AdminContext from './context/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthConteext>
       <AdminContext>
         <App />
       </AdminContext>
     </AuthConteext>
-  </BrowserRouter>,
+  </HashRouter>,
 )
