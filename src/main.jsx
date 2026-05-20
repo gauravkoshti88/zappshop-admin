10e6d9195ac1766b4164dcad69076e08d9ba6a91
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-21 04:05:58 +0530

    Deploy in vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {HashRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AuthConteext from './context/AuthConteext.jsx'
 import AdminContext from './context/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <AuthConteext>
       <AdminContext>
         <App />
       </AdminContext>
     </AuthConteext>
-  </HashRouter>,
+  </BrowserRouter>,
 )
